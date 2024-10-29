a348766d260fa8926a728830bcbfda050001038c
khnsizza <161116803+khnsizza@users.noreply.github.com>
2024-10-30 00:02:03 +0700

    Add files via upload

---
+++ mvctugas/mvc2/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "config.php";
+require_once "Controller.php";
+require_once "Database.php";
+require_once "Model.php";
+require_once "apps/controllers/Home.php";
+require_once "Boot.php";
+
+
+new Boot();
